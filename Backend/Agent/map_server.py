3c1d16b2df5720caeefb7a45ab0ad7a2c0ebda59
Urban ABM Developer <developer@urbanaom.local>
2026-04-04 01:28:18 +0200

    Changed the uvicorn reload to False to accomodate the temp files

--- Backend/Agent/map_server.py
+++ Backend/Agent/map_server.py
@@ -970,4 +970,4 @@ async def download_recording(filename: str):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("map_server:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("map_server:app", host="127.0.0.1", port=8000, reload=False)
